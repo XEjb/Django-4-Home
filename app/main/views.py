6864ef1ed4f230c3c1c61c6e2f369a33f70d73d7
Xejb <vladiq-artamonenko@rambler.ru>
2024-03-07 15:17:38 +0300

    routes

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    context: dict[str, str] = {
+        'title': 'Home',
+        'content': 'Главная страница',
+        'list': ['first', 'second'],
+        'dict': {'first': 1},
+        'bool': True
+    }
+    return render(request, 'main/index.html', context)
+
+
+def about(request):
+    return HttpResponse('About page')
