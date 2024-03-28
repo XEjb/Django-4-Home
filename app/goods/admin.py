1879ea2d64cc1d22e38084abee498c6ca188a0fa
Xejb <vladiq-artamonenko@rambler.ru>
2024-03-28 14:47:49 +0300

    model registration

--- app/goods/admin.py
+++ app/goods/admin.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 
 from goods.models import Categories, Products
 
-admin.site.register(Categories)
-admin.site.register(Products)
+# admin.site.register(Categories)
+# admin.site.register(Products)
 
+
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Products)
+class CategoriesAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
