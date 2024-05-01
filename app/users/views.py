6fa6044558b5612da3e6f681360fcb2106c071bb
Xejb <vladiq-artamonenko@rambler.ru>
2024-05-01 14:12:25 +0300

    Templates for users app added

--- app/users/views.py
+++ app/users/views.py
@@ -5,21 +5,21 @@ def login(request):
     context = {
         'title': 'Home - Авторизация'
     }
-    return render(request, '', context)
+    return render(request, 'users/login.html', context)
 
 
 def registration(request):
     context = {
         'title': 'Home - Регистрация'
     }
-    return render(request, '', context)
+    return render(request, 'users/registration.html', context)
 
 
 def profile(request):
     context = {
         'title': 'Home - Кабинет'
     }
-    return render(request, '', context)
+    return render(request, 'users/profile.html', context)
 
 
 def logout(request):
