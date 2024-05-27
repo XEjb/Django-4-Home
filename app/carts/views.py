e8b3477ed3443c6399d82c4f332417643443ef00
Xejb <vladiq-artamonenko@rambler.ru>
2024-05-27 15:40:19 +0300

    JsonResponse and render to string

--- app/carts/views.py
+++ app/carts/views.py
@@ -1,4 +1,6 @@
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
 
 from goods.models import Products
 
@@ -6,7 +8,6 @@ from carts.models import Cart
 
 
 def cart_add(request):
-
     product_id = request.POST.get('product_id')
     product = Products.objects.get(id=product_id)
 
@@ -21,7 +22,15 @@ def cart_add(request):
         else:
             Cart.objects.create(user=request.user, product=product, quantity=1)
 
-    return redirect(request.META['HTTP_REFERER'])
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html", {"carts": user_cart}, request=request)
+
+    response_data = {
+        "message": "Товар добавлен в корзину",
+        "cart_items_html": cart_items_html,
+    }
+
+    return JsonResponse(response_data)
 
 
 def cart_change(request, product_slug):
