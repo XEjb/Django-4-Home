25f40a0db5ddaf40a9ed9293e9f323d8e371fa85
Xejb <vladiq-artamonenko@rambler.ru>
2024-05-31 13:11:40 +0300

    Adding filters to the admin panel

--- app/carts/admin.py
+++ app/carts/admin.py
@@ -2,4 +2,18 @@ from django.contrib import admin
 
 from carts.models import Cart
 
-admin.site.register(Cart)
+
+# admin.site.register(Cart)
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ["user_display", "product_display", "quantity", "created_timestamp", ]
+    list_filter = ["created_timestamp", "user", "product__name", ]
+
+    def user_display(self, obj):
+        if obj.user:
+            return str(obj.user)
+        return "Анонимный пользователь"
+
+    def product_display(self, obj):
+        return str(obj.product.name)
