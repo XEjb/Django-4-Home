14eaa94c019b57b383a3883da96206f109dfe7d7
Xejb <vladiq-artamonenko@rambler.ru>
2024-08-07 16:18:01 +0300

    url

--- app/goods/urls.py
+++ app/goods/urls.py
@@ -7,5 +7,5 @@ app_name = 'goods'
 urlpatterns = [
     path('search/', views.catalog, name='search'),
     path('<slug:category_slug>/', views.catalog, name='index'),
-    path('product/<slug:product_slug>/', views.product, name='product'),
+    path('product/<slug:product_slug>/', views.ProductView.as_view(), name='product'),
 ]
