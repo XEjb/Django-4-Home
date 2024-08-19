39ccdc551c0207cd171e238f99767b21d5116c8a
Xejb <vladiq-artamonenko@rambler.ru>
2024-08-19 16:24:49 +0300

    urls

--- app/users/urls.py
+++ app/users/urls.py
@@ -6,7 +6,7 @@ app_name = 'users'
 
 urlpatterns = [
     path('login/', views.UserLoginView.as_view(), name='login'),
-    path('registration/', views.registration, name='registration'),
+    path('registration/', views.UserRegistrationView.as_view(), name='registration'),
     path('profile/', views.profile, name='profile'),
     path('users-cart/', views.users_cart, name='users_cart'),
     path('logout/', views.logout, name='logout'),
